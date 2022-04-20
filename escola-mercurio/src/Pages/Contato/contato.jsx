37baa309e50afbd967949025c63e520d96e53a8d
yagormorares <yago.dmoraes@gmail.com>
2022-04-20 12:12:52 -0300

    Implementação da Pagina Contato no React

---
+++ escola-mercurio/src/Pages/Contato/contato.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Contato() {
+    return(
+        <form action="">
+            <h1>Formulario Teste - Contato</h1>
+            <input type="text" />
+            <label htmlFor="">Label de Teste1</label>
+            
+            <input type="text" />
+            <label htmlFor="">Label de Teste2</label>
+
+            <input type="text" />
+            <label htmlFor="">Label de Teste3</label>
+        </form>
+    )
+}
+
+export default Contato
