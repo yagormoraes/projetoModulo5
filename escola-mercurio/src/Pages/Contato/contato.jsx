796dec76e4a7de186b2d9b83cd292a6837e5ffd9
yagormorares <yago.dmoraes@gmail.com>
2022-04-20 13:53:29 -0300

    Utilização do formsubmit.co

--- escola-mercurio/src/Pages/Contato/contato.jsx
+++ escola-mercurio/src/Pages/Contato/contato.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 function Contato() {
     return(
-        <form action="">
+        <form action="https://formsubmit.co/yago.dmoraes@gmail.com" method='POST'>
             <h1>Formulario Teste - Contato</h1>
-            <input type="text" />
-            <label htmlFor="">Label de Teste1</label>
+            <label htmlFor="text1">Label de Teste1</label>
+            <input type="text" name='text1' id='text1'/>
             
-            <input type="text" />
-            <label htmlFor="">Label de Teste2</label>
+            <label htmlFor="text2">Label de Teste2</label>
+            <input type="text" name='text2' id='text2'/>
 
-            <input type="text" />
-            <label htmlFor="">Label de Teste3</label>
+            <label htmlFor="text3">Label de Teste3</label>
+            <input type="text" name='text3' id='text3'/>
+
+            <button type="submit">Send</button>
         </form>
     )
 }
