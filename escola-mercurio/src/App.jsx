59aa5ffa20d7b816c2ca9b91efb8fab257a91618
yagormorares <yago.dmoraes@gmail.com>
2022-04-26 22:16:47 -0300

    Correção do Bootstrap

--- escola-mercurio/src/App.jsx
+++ escola-mercurio/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
