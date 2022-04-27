38a5034cc7ffcbbc88bede876585495d8661043d
FilpeRibeiro <filipe-negreti@hotmail.com>
2022-04-26 22:15:21 -0300

    descriçao das imagens do segmento

--- escola-mercurio/src/Pages/Segmentos/segmentos.jsx
+++ escola-mercurio/src/Pages/Segmentos/segmentos.jsx
@@ -21,7 +21,7 @@ function Segmentos (){
              <h1 className='titulo'>
                  Metodologia - Fundamental ( I e II )
              </h1>
-             <img className='mt-5 img' src={Fundamental} />
+             <img className='mt-5 img' src={Fundamental} alt="crianças na creche"/>
              <p  className='mt-5' style={Texto}>
              Princípios como o diálogo, a colaboração, o pensamento crítico, a criatividade e a pesquisa estão na base do trabalho desenvolvido no Ensino Fundamental I. Nesse segmento, diferentes metodologias e estratégias de ensino são usadas para ampliar o repertório dos alunos e desenvolver diversas habilidades e competências.
              Desde pequenos, nossos alunos são incentivados a construírem a própria autonomia e a exercerem a autoria de suas produções. Em diferentes ambientes, alunos e professores altamente qualificados protagonizam uma aprendizagem ativa e significativa, em diálogo com a cultura, com o conhecimento científico e a tecnologia.
@@ -33,7 +33,7 @@ function Segmentos (){
              <h1 className='titulo'>
                  Metodologia - Ensino Medio
              </h1>
-             <img className='mt-5  img' src={Medio} />
+             <img className='mt-5  img' src={Medio}  alt="Crianças no ensino medio"/>
              <p className='mt-5' style={Texto}>
              O nosso Ensino Médio é construído a partir de uma formação produtiva e responsável com foco no desenvolvimento do autoconhecimento e do potencial do aluno. Valorizamos as experiências individuais e coletivas para que cada estudante se torne um indivíduo crítico, participativo e dialógico, aplicando seus conhecimentos na resolução de problemas de forma autônoma e proativa. Com um corpo de professores altamente qualificado, propostas interdisciplinares, metodologias ativas e formação tecnológica, preparamos nossos alunos para enfrentar os grandes desafios da vida, do pleno exercício da cidadania e do mundo do trabalho.
              </p>
@@ -42,7 +42,7 @@ function Segmentos (){
              <h1 className='titulo'>
                  Metodologia - Vestibular
              </h1>
-             <img className='mt-5 img' src={Vestibular} />
+             <img className='mt-5 img' src={Vestibular} alt="cursinho para vestibular"/>
              <p  className='mt-5'style={Texto}>
              Com o objetivo de preparar os alunos para as provas de ingresso no Ensino Superior mais importantes do país, desenvolvemos diferentes metodologias, projetos e simulados. Além disso, temos um Coordenador de Vestibular que acompanha os estudantes ao longo do ano e os auxilia nos estudos e preparação para o Enem e vestibulares.
              </p>
