a81369b9f34f8326c58169147d0886e561469535
yagormorares <yago.dmoraes@gmail.com>
2022-05-02 12:22:41 -0300

    Alterações de CSS na aba professor

--- escola-mercurio/src/Components/listaSeries/ListaSeries.jsx
+++ escola-mercurio/src/Components/listaSeries/ListaSeries.jsx
@@ -4,14 +4,14 @@ import { Container } from "./listaSeriesStyled.jsx";
 
 function ListaSeries({matricula,nome,email}) {
   return (
-    // Criamos uma estilização com styled feita em arquivo separado e chamada aqui como Container
+
     <Container>
       <Link to={"/visualizar/" + matricula}>
           <p>{nome}</p>
           <p>{email}</p>
           
       </Link>
-      {/* <Button nome="Deletar" click={handleDelete} /> */}
+
     </Container>
   );
 }
